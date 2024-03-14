aeb48c223394cbf282caad9e485a081be00512a4
MatteoStrazzera <mattestrazz@gmail.com>
2024-03-14 12:43:27 +0100

    add dynamic contact avatar and name with v-for(milestone 1)

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ createApp({
             contacts: [
                 {
                     name: 'Michele',
-                    avatar: './assets/img/avatar_1.png',
+                    avatar: './assets/img/avatar_1.jpg',
                     visible: true,
                     messages: [
                         {
@@ -30,7 +30,7 @@ createApp({
                 },
                 {
                     name: 'Fabio',
-                    avatar: './assets/img/avatar_2.png',
+                    avatar: './assets/img/avatar_2.jpg',
                     visible: true,
                     messages: [
                         {
@@ -52,7 +52,7 @@ createApp({
                 },
                 {
                     name: 'Samuele',
-                    avatar: './assets/img/avatar_3.png',
+                    avatar: './assets/img/avatar_3.jpg',
                     visible: true,
                     messages: [
                         {
@@ -74,7 +74,7 @@ createApp({
                 },
                 {
                     name: 'Alessandro B.',
-                    avatar: './assets/img/avatar_4.png',
+                    avatar: './assets/img/avatar_4.jpg',
                     visible: true,
                     messages: [
                         {
@@ -91,7 +91,7 @@ createApp({
                 },
                 {
                     name: 'Alessandro L.',
-                    avatar: './assets/img/avatar_5.png',
+                    avatar: './assets/img/avatar_5.jpg',
                     visible: true,
                     messages: [
                         {
@@ -108,7 +108,7 @@ createApp({
                 },
                 {
                     name: 'Claudia',
-                    avatar: './assets/img/avatar_6.png',
+                    avatar: './assets/img/avatar_6.jpg',
                     visible: true,
                     messages: [
                         {
@@ -130,7 +130,7 @@ createApp({
                 },
                 {
                     name: 'Federico',
-                    avatar: './assets/img/avatar_7.png',
+                    avatar: './assets/img/avatar_7.jpg',
                     visible: true,
                     messages: [
                         {
@@ -147,7 +147,7 @@ createApp({
                 },
                 {
                     name: 'Davide',
-                    avatar: './assets/img/avatar_8.png',
+                    avatar: './assets/img/avatar_8.jpg',
                     visible: true,
                     messages: [
                         {
@@ -171,6 +171,6 @@ createApp({
         }
     },
     mounted(){
-        console.log(this.contacts);
+        console.log(this.contacts[0].name);
     }
 }).mount('#app')
